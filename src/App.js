74f701255c95e12547dfded87e3fcdf558917526
Sicelukwanda Zwane <sicelukwanda.zwane@gmail.com>
2024-12-08 20:22:31 +0000

    Fix malformed class issues

--- src/App.js
+++ src/App.js
@@ -25,14 +25,14 @@ function App() {
       }}
     >
       <header className="App-header">
-        <h2 class="fixed-name">Sicelukwanda Zwane</h2>
+        <h2 className="fixed-name">Sicelukwanda Zwane</h2>
         <h1>About me</h1>
         <img src={`${process.env.PUBLIC_URL}/images/profile-photo.jpg`} alt="Profile" className="profile-photo"/>
         <p>
           {/* My recent reach interests include planning under uncertainty, robotics for the circular economy, soft robotics, and planning with diffusion. */}
           I am a PhD student at 
           the <a href="https://www.ucl.ac.uk/">UCL</a>, <a href="https://www.ucl.ac.uk/ai-centre/">Centre for Artificial Intelligence</a>, 
-          where I work on developing data-efficient and safety-aware machine learning algorithms for robotics. I am part of the <a href="https://www.sml-group.cc">Sustainability and Machine Learning Group</a> as well as the
+          where I work on developing data-efficient and safety-aware machine learning algorithms for robotics. I am part of the <a href="https://www.sml-group.cc">Sustainability and Machine Learning Group</a> as well as
           the <a href="https://rpl-as-ucl.github.io/">Robot Perception and Learning Lab</a>.
         </p>
         <p>
@@ -70,7 +70,7 @@ function App() {
         here</a>
       </h4>
       <ExperienceTimeline experiences={experiencesData}/>
-      <div class="attribution">
+      <div className="attribution">
         <h6>Copyright © 2024 Sicelukwanda Zwane. All Rights Reserved. &nbsp;</h6>
       </div> 
     </div>
